6208d75cecb7c2f20da8a9f419ae1ebf841da46e
Rishi Kant <kantrishi7779@gmail.com>
2025-10-09 21:15:04 +0530

    added page.tsx

---
+++ apps/web/app/make/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Page() {
+    return (
+        <div className="h-screen w-screen relative overflow-hidden flex flex-col">
+            hey
+        </div>
+    );
+}
