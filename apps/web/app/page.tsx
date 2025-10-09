72e9f0bea52cbb9e01c8b9667179ff4eccb1d738
Rishi Kant <kantrishi7779@gmail.com>
2025-10-09 23:32:42 +0530

    added image for landing page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,11 @@
 import Navbar from "@/src/components/nav/Navbar";
+import Image from "next/image";
 
 export default function Page() {
     return (
         <div className="h-screen w-screen relative overflow-hidden flex flex-col">
             <Navbar />
+            <Image src="/images/base.png" alt="logo" fill className="" />
         </div>
     );
 }
