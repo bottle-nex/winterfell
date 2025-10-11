75177139de79977daf8eb2b1dd2594c7123b82d0
piyush-rj <piyushraj26102004@gmail.com>
2025-10-11 16:46:39 +0530

    errors resolved

---
+++ apps/web/src/components/base/Features.tsx
@@ -0,0 +1,44 @@
+"use client";
+import Image from "next/image";
+
+export default function Features() {
+    return (
+        <section className="w-full flex flex-col md:flex-row max-w-6xl mx-auto py-20 px-4 gap-x-16 relative">
+
+            <div className="md:w-1/3 flex-shrink-0">
+                <div className="md:sticky md:top-24 font-bold text-5xl leading-[1.2] text-[#F2EFEC] whitespace-nowrap flex flex-col">
+                    <span>SIMPLIFY WRITING</span>
+                    <span>CONTRACTS</span>
+                </div>
+            </div>
+
+            <div className="md:w-2/3 flex flex-col space-y-15">
+                {[1, 2, 3].map((i) => (
+                    <div
+                        key={i}
+                        className="h-[400px] w-full md:w-[500px] shadow-xl rounded-[8px] p-5 space-y-2 bg-[#0f0f0f] mx-auto"
+                    >
+                        <div className="w-full h-[65%] border border-neutral-700 rounded-[4px] relative overflow-hidden">
+                            <Image
+                                src={"/Images/vscode.png"}
+                                alt=""
+                                fill
+                                className="object-cover"
+                                unoptimized
+                            />
+                        </div>
+                        <div className="w-full h-[35%] space-y-2 py-2 flex flex-col justify-center">
+                            <div className="text-[22px] font-extralight tracking-wider text-[#c2c2c2]">
+                                Give Inputs
+                            </div>
+                            <div className="text-[#a5a3a1] text-sm tracking-wide">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum,
+                                sint fugit! Sunt assumenda laudantium.
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </section>
+    );
+}
--- apps/web/src/components/base/Hero.tsx
+++ apps/web/src/components/base/Hero.tsx
@@ -1,41 +1,52 @@
+"use client";
+import { useState, useEffect } from "react";
+import AnimatedCircleText from "@/src/components/base/AnimatedCircleText";
+import Navbar from "@/src/components/nav/Navbar";
+import { Button } from "@/src/components/ui/button";
 import { ChevronRight } from "lucide-react";
-import { Button } from "../ui/button";
+import CircularDotsCanvas from "./CircularDotCanvas";
 
 export default function Hero() {
+    const [scrolled, setScrolled] = useState<boolean>(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setScrolled(window.scrollY > 10);
+        };
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
     return (
-        <div className="flex flex-col gap-y-3 pl-10 select-none relative">
+        <main className="relative flex flex-col justify-between items-center h-screen w-full bg-black rounded-[8px] overflow-hidden mt-9 shadow-[0_-2px_30px_rgba(0,0,0,0.5)]">
 
-            <span className="tracking-wide drop-shadow-2xl">THE EASIEST WAY</span>
+            <CircularDotsCanvas />
 
-            <span>
-                <span className="tracking-wide">TO BUILD</span>
-                <span className="tracking-wide">{" "}SMART</span>
-                <span className="tracking-wide drop-shadow-2xl">{" "}CONTRACTS</span>
-            </span>
-
-            <span className="tracking-wide">DEPLOY SMARTER</span>
-
-            <div className="flex gap-x-4 items-center">
-                <span className="tracking-wide text-stroke">NOT HARDER</span>
-
-                <div className="relative overflow-hidden rounded-full bg-primary text-dark-base px-7 h-12 flex items-center w-40">
-                    <div className="flex animate-marquee gap-x-4 whitespace-nowrap">
-                        {[...Array(14)].map((_, i) => (
-                            <ChevronRight key={i} />
-                        ))}
-                    </div>
-                </div>
+            <div className="absolute h-[900px] w-[900px] rounded-full -top-80 -right-40 flex justify-center items-center">
+                <div className="h-[700px] w-[700px] rounded-full" />
             </div>
-            <style jsx>{`
-        @keyframes marquee {
-          0% { transform: translateX(-50%); }
-          100% { transform: translateX(0%); }
-        }
-        .animate-marquee {
-          display: inline-flex;
-          animation: marquee 5s linear infinite;
-        }
-      `}</style>
-        </div>
+
+            <div
+                className={`w-full flex fixed justify-center items-start pt-2 z-10 px-5 transition-all duration-300 ${scrolled ? "backdrop-blur-xs -translate-y-10" : ""
+                    }`}
+            >
+                <Navbar />
+            </div>
+
+            <div className="flex-1 w-full flex justify-center items-center z-10"></div>
+
+            <div className="absolute bottom-12 right-5 z-1">
+                <Button className="flex items-center bg-[#FDF9EF] text-[#080808] hover:bg-[#eae6db] transition-all duration-300 group rounded-full px-6 py-3">
+                    Redefine smart contracts
+                    <span className="ml-1 transition-transform duration-300 group-hover:translate-x-1">
+                        <ChevronRight />
+                    </span>
+                </Button>
+            </div>
+
+            <div className="absolute inset-0 bottom-7 flex justify-center items-center z-0 pointer-events-none">
+                <AnimatedCircleText />
+            </div>
+        </main>
     );
 }
