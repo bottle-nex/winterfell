bf2d228981944fa72e2eb092e3111afe90ca22ab
piyush-rj <piyushraj26102004@gmail.com>
2025-10-14 11:52:12 +0530

    feat: frontend parser + cloudfront integration + backend get controller

--- apps/server/src/routes/index.ts
+++ apps/server/src/routes/index.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import signInController from '../controllers/user-controller/signInController';
 import startChatController from '../controllers/chat-controller/startChatController';
+import getFilesController from '../controllers/chat-controller/getFilesController';
 
 const router: Router = Router();
 
 // user-routes
 router.post('/sign-in', signInController);
 router.post('/new', startChatController);
+router.get('/files/:contractId', getFilesController);
+
 export default router;
