567dd470a4be4b5bd0ed775897e41f8076381544
Anjansuman <anjansuman80@gmail.com>
2025-10-16 00:49:34 +0530

    feat: added order creation and subscription updation routes.

---
+++ apps/server/src/controllers/payment-controller/createOrderController.ts
@@ -0,0 +1,70 @@
+import { Request, Response } from "express";
+import PLANS from "../../configs/plans";
+import { razorpay } from "../../services/init";
+import { prisma, SubscriptionStatus } from "@repo/database";
+
+export default async function createOrderController(req: Request, res: Response) {
+    try {
+
+        const { planType } = req.body;
+        const user = req.user;
+
+        if (!user) {
+            res.status(401).json({
+                success: false,
+                message: 'Unauthorized',
+            });
+            return;
+        }
+
+        const plan = PLANS[planType as keyof typeof PLANS];
+        if (!plan) {
+            res.status(400).json({
+                success: false,
+                message: 'Invalid plan',
+            });
+            return;
+        }
+
+        const order = await razorpay.create_order(planType, user.email);
+
+        // upserting the subscription in the DB
+        await prisma.subscription.upsert({
+            where: {
+                userId: user.id,
+            },
+            update: {
+                plan: planType,
+                status: SubscriptionStatus.PENDING,
+                razorpayOrderId: order.id,
+            },
+            create: {
+                userId: user.id,
+                plan: planType,
+                status: SubscriptionStatus.PENDING,
+                razorpayOrderId: order.id,
+            },
+        });
+
+        res.status(201).json({
+            success: true,
+            message: 'Order created successfully',
+            plan: {
+                plan: planType,
+                orderId: order.id,
+                amount: plan.amount,
+                currency: plan.currency,
+                interval: plan.interval,
+            }
+        });
+        return;
+
+    } catch (err) {
+        console.error('Error while creating order: ', err);
+        res.status(500).json({
+            success: false,
+            message: 'Internal server error',
+        });
+        return;
+    }
+}
---
+++ apps/server/src/controllers/payment-controller/updateSubscriptionController.ts
@@ -0,0 +1,65 @@
+import { Request, Response } from "express";
+import { razorpay } from "../../services/init";
+import { prisma } from "@repo/database";
+
+
+export default async function updateSubscriptionController(req: Request, res: Response) {
+    try {
+        
+        const { paymentId, signature } = req.body;
+        const user = req.user;
+
+        if(!user) {
+            res.status(401).json({
+                success: false,
+                message: 'Unauthorized',
+            });
+            return;
+        }
+
+        const userSubscription = await prisma.subscription.findUnique({
+            where: {
+                userId: user.id,
+            },
+        });
+
+        if(!userSubscription || userSubscription.razorpayOrderId) {
+            res.status(404).json({
+                success: false,
+                message: 'No subscription found',
+            });
+            return;
+        }
+
+        const { success, message } = await razorpay.verify_and_update(
+            userSubscription.razorpayOrderId!,
+            paymentId,
+            signature,
+            userSubscription.id,
+            user.id,
+            userSubscription.plan,
+        );
+
+        if(!success) {
+            res.status(401).json({
+                success: false,
+                message: message
+            });
+            return;
+        }
+
+        res.status(201).json({
+            success: true,
+            message: 'Subscription updated successfully',
+        });
+        return;
+        
+    } catch (error) {
+        console.error('Error while verifying payment: ', error);
+        res.status(500).json({
+            success: false,
+            message: 'Internal server error',
+        });
+        return;
+    }
+}
