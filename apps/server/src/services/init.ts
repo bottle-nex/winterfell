3c5b8dba73239f4e9c77a26d5677956112958020
Anjansuman <anjansuman80@gmail.com>
2025-10-16 00:23:23 +0530

    feat: added razorpay abstraction.

--- apps/server/src/services/init.ts
+++ apps/server/src/services/init.ts
@@ -1,10 +1,15 @@
+import Razorpay from 'razorpay';
 import ObjectStore from '../class/object_store';
 import ContentGenerator from '../controllers/gen/content_generator';
+import env from '../configs/env';
+import RazorpayGateway from '../payments/razorpay';
 
 export let contentGenerator: ContentGenerator;
 export let objectStore: ObjectStore;
+export let razorpay: RazorpayGateway;
 
 export default function init_services() {
     contentGenerator = new ContentGenerator();
     objectStore = new ObjectStore();
+    razorpay = new RazorpayGateway();
 }
