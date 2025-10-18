b03c7aaba13b9fcbe81d896c05393229bbe8019e
Anjansuman <anjansuman80@gmail.com>
2025-10-19 01:03:33 +0530

    added: queues.

---
+++ apps/server/src/queue/loader_queue.ts
@@ -0,0 +1,24 @@
+import { createClient, RedisClientType } from "redis";
+
+export class ServerToOrchestratorQueue {
+    private client: RedisClientType;
+    private queue_name: string;
+
+    constructor(queue_name: string) {
+        this.queue_name = queue_name;
+        this.client = createClient({
+            url: 'redis://localhost:6379',
+        });
+        this.client.connect();
+    }
+
+    async push(data: any) {
+        const payload = JSON.stringify(data);
+        await this.client.lPush(this.queue_name, payload);
+    }
+
+    disconnect() {
+        this.client.quit();
+    }
+
+}
