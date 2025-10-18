f626af74c352ccbe11cfd7d15f078581a2120bc9
Anjansuman <anjansuman80@gmail.com>
2025-10-18 15:54:49 +0530

    feat: added generalized debounce and changing file data debouncer.

--- apps/web/src/types/prisma-types.d.ts
+++ apps/web/src/types/prisma-types.d.ts
@@ -3,3 +3,17 @@ export enum PlanType {
     PREMIUM = 'PREMIUM',
     PREMIUM_PLUS = 'PREMIUM_PLUS',
 }
+
+export enum NODE {
+    FILE = 'FILE',
+    FOLDER = 'FOLDER',
+}
+
+export interface FileNode {
+    id: string;
+    name: string;
+    type: NODE;
+    content?: string;
+    language?: string;
+    children?: FileNode[];
+}
