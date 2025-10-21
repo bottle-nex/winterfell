a4a3a31f81f9c5174a9c36ed4c2076d7fa4cd1d8
Rishi Kant <kant-mac@Rishis-MacBook-Air.local>
2025-10-21 12:54:49 +0530

    build fixes

--- apps/kubernetes/src/index.ts
+++ apps/kubernetes/src/index.ts
@@ -5,10 +5,6 @@ import getPodStatusController from './controllers/get_pod_status_controller';
 import deletePodController from './controllers/delete_pod_controller';
 import { env } from './configs/env.config';
 import { logger } from './utils/logger';
-import { writeFileTree } from './parser/file_node_to_file_parser';
-import { fileTree } from './test/test';
-import * as path from 'path';
-
 const app = express();
 app.use(express.json());
 const PORT = env.KUBERNETES_PORT;
@@ -18,8 +14,6 @@ export const podService = new PodService();
 app.post('/create-pod', createPodController);
 app.get('/get-pod-status', getPodStatusController);
 app.delete('/delete-pod', deletePodController);
-
-writeFileTree(fileTree, path.join(process.cwd(), 'test-project'));
 
 app.listen(PORT, () => {
    logger.info(`Orchestrator running on port ${PORT}`);
