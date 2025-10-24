ddc0dc85973051c09e4b2ac6ef0b1af212076101
Anjansuman <anjansuman80@gmail.com>
2025-10-25 01:53:03 +0530

    client code for commands.

--- apps/web/src/components/code/TerminalCommands.ts
+++ apps/web/src/components/code/TerminalCommands.ts
@@ -21,15 +21,17 @@ shell               winterfell
 `;
 
 const commandsResponse = `
-WINTERFELL SHELL COMMANDS:
+SHELL COMMANDS:
 winterfell build                to build the contract
 winterfell test                 to run the test file
 
 
-PREMIUM FEATURES:
+PREMIUM(+) SHELL COMMANDS:
 winterfell deploy --devnet      to deploy the contract on devnet
 winterfell deploy --mainnet     to deploy the contract on mainnet
 `;
+
+const winterfellBuildResponse = ``;
 
 export enum COMMAND {
     CLEAR = 'clear',
@@ -37,6 +39,10 @@ export enum COMMAND {
     HOT_KEYS = '--hotkeys',
     PLATFORM = '--platform',
     COMMANDS = '--commands',
+    WINTERFELL_BUILD = 'winterfell build',
+    WINTERFELL_TEST = 'winterfell test',
+    WINTERFELL_DEPLOY_DEVNET = 'winterfell deploy --devnet',
+    WINTERFELL_DEPLOY_MAINNET = 'winterfell deploy --mainnet',
 }
 
 export const CommandResponse: Record<COMMAND, string> = {
@@ -45,4 +51,8 @@ export const CommandResponse: Record<COMMAND, string> = {
     [COMMAND.HOT_KEYS]: hotKeysResponse,
     [COMMAND.PLATFORM]: platformResponse,
     [COMMAND.COMMANDS]: commandsResponse,
+    [COMMAND.WINTERFELL_BUILD]: `sure I'll start building your contract`,
+    [COMMAND.WINTERFELL_TEST]: `sure I'll start testing you contract`,
+    [COMMAND.WINTERFELL_DEPLOY_DEVNET]: `sure I'll start deploying your contract to devnet`,
+    [COMMAND.WINTERFELL_DEPLOY_MAINNET]: `sure I'll start deploying you contract to mainnet`,
 };
