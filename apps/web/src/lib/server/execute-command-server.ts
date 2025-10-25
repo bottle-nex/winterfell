1dba5ec83d3eb5a184172f8e8a60153920270163
kant-github <kantrishi7779@gmail.com>
2025-10-25 16:19:59 +0530

    kubernetes command execution completed

--- apps/web/src/lib/server/execute-command-server.ts
+++ apps/web/src/lib/server/execute-command-server.ts
@@ -1,16 +1,24 @@
 import { RUN_COMMAND_URL } from '@/routes/api_routes';
 import axios from 'axios';
 
-export default async function executeCommandServer(command: string, contractId: string, token: string) {
+export default async function executeCommandServer(
+    command: string,
+    contractId: string,
+    token: string,
+) {
     try {
-        const res = await axios.post(RUN_COMMAND_URL, {
-            command: command,
-            contractId: contractId,
-        },{
-            headers: {
-                Authorization: `Bearer ${token}`,
+        const res = await axios.post(
+            RUN_COMMAND_URL,
+            {
+                command: command,
+                contractId: contractId,
             },
-        });
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            },
+        );
 
         return res.data;
     } catch (error) {}
