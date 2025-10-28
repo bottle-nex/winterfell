37af8aa850da623eb904cd013586001158d82349
kant-github <kantrishi7779@gmail.com>
2025-10-28 05:45:03 +0530

    added dockerfile for amd64 anchor base image installation

--- apps/server/src/types/github_worker_queue_types.ts
+++ apps/server/src/types/github_worker_queue_types.ts
@@ -1,13 +1,13 @@
 export enum GITHUB_WORKER_QUEUE_TYPES {
-    GITHUB_EXPORT = 'GITHUB_EXPORT'
+    GITHUB_EXPORT = 'GITHUB_EXPORT',
 }
 
 export interface GithubPushJobData {
-  github_access_token: string;
-  owner: string;
-  repo_name: string;
-  user_id: string;
-  contract_id: string;
+    github_access_token: string;
+    owner: string;
+    repo_name: string;
+    user_id: string;
+    contract_id: string;
 }
 
 export interface FileContent {
