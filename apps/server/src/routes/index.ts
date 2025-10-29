c280c7299eb2b16802fc034186549672ce7c3d40
piyush-rj <piyushraj26102004@gmail.com>
2025-10-29 11:16:59 +0530

    github code push controller and queue fixed

--- apps/server/src/routes/index.ts
+++ apps/server/src/routes/index.ts
@@ -9,7 +9,8 @@ import subscriptionMiddleware from '../middlewares/subscriptionMiddleware';
 import getUserPlanController from '../controllers/payment-controller/getUserPlanController';
 import syncFilesController from '../controllers/files/syncFilesController';
 import runCommandController from '../controllers/contract-controller/runCommandController';
-import { githubCodePushController } from '../controllers/github-deploy-controller/githubCodePushController';
+import githubCodePushController from '../controllers/github-deploy-controller/githubCodePushController';
+import githubJobStatusController from '../controllers/github-deploy-controller/githubJobStatusController';
 
 const router: Router = Router();
 
@@ -23,6 +24,7 @@ router.get('/health', (_req: Request, res: Response) => {
 router.post('/new', authMiddleware, startChatController);
 router.post('/contract/run-command', authMiddleware, runCommandController);
 router.post('/contract/export', authMiddleware, githubCodePushController);
+router.get('/contract/push-status', githubJobStatusController);
 
 // file-routes
 router.get('/files/:contractId', authMiddleware, getFilesController);
