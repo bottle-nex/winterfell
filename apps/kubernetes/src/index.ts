284081d18b6f46ecb40b51972b56b40df351f9c3
kant-github <kantrishi7779@gmail.com>
2025-11-05 01:12:49 +0530

    re initialized kubernetes server

--- apps/kubernetes/src/index.ts
+++ apps/kubernetes/src/index.ts
@@ -1,12 +1,1 @@
-import express from 'express';
-import { env } from './configs/env.config';
-import { logger } from './utils/logger';
-import { init_services } from './services/init_services';
-
-const app = express();
-app.use(express.json());
-
-init_services();
-const PORT = env.KUBERNETES_PORT;
-
-app.listen(PORT);
+console.log("hey");
