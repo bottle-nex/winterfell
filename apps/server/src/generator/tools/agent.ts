b403956eecb18b97a97653fe3a09143bc2b7db1f
Anjansuman <anjansuman80@gmail.com>
2025-11-19 22:16:42 +0530

    added agent and agent tools.

--- apps/server/src/generator/tools/agent.ts
+++ apps/server/src/generator/tools/agent.ts
@@ -1,40 +1,115 @@
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import env from "../../configs/config.env";
 import { MessagesAnnotation, StateGraph } from "@langchain/langgraph";
-import { gemini_coder } from "../models/models";
 import Tool from "./tool";
+import { AIMessage } from "@langchain/core/messages";
 
-async function llm_call(state: typeof MessagesAnnotation.State) {
-    const result = await gemini_coder.invoke([
-        {
-            role: 'system',
-            content: `you're a expert anchor solana contract writer.`
-        },
-        ...state.messages,
-    ]);
 
-    return {
-        messages: [result]
+export default class Agent {
+
+    private llm;
+
+    constructor() {
+        this.llm = new ChatGoogleGenerativeAI({
+            model: 'gemini-2.5-flash',
+            temperature: 0.2,
+            streaming: true,
+            apiKey: env.SERVER_GEMINI_API_KEY,
+        });
+        // add the clause llm too
     }
+
+    public final_call() {
+        const messages = [{
+            role: 'user',
+            content: 'create a todo contract',
+        }];
+        
+        const final = async () => {
+            console.log('here comes the ai call and response');
+            const result = await this.agent_builder.invoke({ messages });
+            console.log(result);
+        }
+        final();
+    }
+
+    private async llm_call(state: typeof MessagesAnnotation.State) {
+        const stream = await this.llm.stream([
+            {
+                role: "system",
+                content: this.coder_content,
+            },
+            ...state.messages,
+        ]);
+
+        let final_content: string = "";
+        let tool_calls: string | any[] | undefined = [];
+
+        for await (const chunk of stream) {
+            if(chunk.content) {
+                console.log(chunk.content);
+                final_content += chunk.content;
+            }
+            if(chunk.tool_calls) {
+                tool_calls = chunk.tool_calls;
+            }
+        }
+
+        const message = new AIMessage({
+            content: final_content,
+            tool_calls: tool_calls.length > 0 ? tool_calls : undefined,
+        });
+
+        return {
+            messages: [message],
+        };
+    }
+
+    private has_tool_calls(msg: any): msg is { tool_calls: any[] } {
+        return Array.isArray(msg?.tool_calls);
+    }
+
+    private should_continue(state: typeof MessagesAnnotation.State) {
+        const last_message = state.messages.at(-1);
+        if(last_message && this.has_tool_calls(last_message)) return "toolNode";
+        return "__end__";
+    }
+
+    public agent_builder = new StateGraph(MessagesAnnotation)
+        .addNode("llmCall", this.llm_call)
+        .addNode("toolNode", Tool.node)
+        .addEdge("__start__", "llmCall")
+        .addConditionalEdges(
+            "llmCall",
+            this.should_continue,
+            ["toolNode", "__end__"],
+        )
+        .addEdge("toolNode", "llmCall")
+        .compile();
+
+    private coder_content = `
+You are a senior Anchor Solana smart contract developer.
+
+IF YOU need rules, CALL get_rule({ rule_name }) **using ONLY EXACT NAMES from above**.
+NEVER invent your own rule names.
+
+
+TOOLS:
+- You can call "get_rule" anytime you need coding guidelines.
+- The argument is: { rule_name: string }
+
+RULE:
+If user asks for a contract, and you need any rule, ALWAYS call the tool.
+Do not guess rules.
+
+AVAILABLE RULES:
+at first do fetch staging_schema for understanding the staging structure
+${Tool.get_rules_name()}
+
+Process:
+1. Think if rules are needed. If yes → call the tool.
+2. Wait for tool output.
+3. Continue generating the contract with correct rules.
+`
+
 }
-
-function has_tool_calls(msg: any): msg is { tool_calls: any[] } {
-    return Array.isArray(msg?.tool_calls);
-}
-
-function shouldContinue(state: typeof MessagesAnnotation.State) {
-    const lastMessage = state.messages.at(-1);
-
-    if(lastMessage && has_tool_calls(lastMessage)) return "toolNode";
-    return "__end__";
-}
-
-const agentBuilder = new StateGraph(MessagesAnnotation)
-    .addNode("llmCall", llm_call)
-    .addNode("toolNode", Tool.node)
-    .addEdge("__start__", "llmCall")
-    .addConditionalEdges(
-        "llmCall",
-        shouldContinue,
-        ["llmCall", "__end__"],
-    )
-    .addEdge("toolNode", "llmCall")
-    .compile();
--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -3,7 +3,8 @@ import http from 'http';
 import env from './configs/config.env';
 import cors from 'cors';
 import router from './routes';
-import init_services, { contract_services } from './services/init';
+import init_services from './services/init';
+import Agent from './generator/tools/Agent';
 
 const app = express();
 const server = http.createServer(app);
@@ -23,15 +24,5 @@ server.listen(env.SERVER_PORT, () => {
     console.warn('Server is running on port : ', env.SERVER_PORT);
 });
 
-const caller = async () => {
-    const result = await contract_services.generate_contract(
-        'build a todo contract with only add todo function.',
-    );
-    console.log('----------------------- the plan');
-    console.log(result.plan);
-    console.log('----------------------- the code');
-    console.log(result.coder_output);
-    console.log('----------------------IGNORE THIS FOR NOW----------------------');
-};
-
-setTimeout(caller, 1000);
+const agent = new Agent();
+agent.final_call();
